ca1bb10846b7e59d697863be74341bbf0bee8ed5
Ankit Khedekar <ankit.khedekar@lrn.com>
2017-11-04 12:42:13 +0530

    search feature uses product service

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../models/products';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'search-bar',
@@ -10,17 +11,27 @@ export class SearchBarComponent implements OnInit {
 
   @Output() public search: EventEmitter<Product[]> = new EventEmitter<Product[]>();
 
-  constructor() { }
+  constructor(private productSvc: ProductService) { }
 
   ngOnInit() {
   }
 
   public searchData(searchText) {
-    const products = [
-      {id: 101, name: 'Bread-Butter', price: 12, mfgDate: '2017-11-08', categoryId: 1},
-      {id: 102, name: 'Bisleri', price: 69, mfgDate: '2017-11-11', categoryId: 2}];
-
-    this.search.emit(products);
+    let products: Product[] = [];
+    this.productSvc.getProducts().subscribe(
+      data => {
+        console.log('got data->', data);
+        const temp: Product[] = [];
+        data.forEach((p) => {
+          if (p.name.toLocaleLowerCase().startsWith(searchText)) {
+            temp.push(p);
+          }
+        });
+        products = temp;
+        this.search.emit(products);
+      },
+      error => console.log(error)
+    );
   }
 
 }
